916252108998271f5dbfe1392f68f12163fee188
Abhishek Kumar <91116774+Abhis-45@users.noreply.github.com>
2025-01-30 15:21:25 +0530

    Update Otp.js

--- frontend/src/pages/Otp.js
+++ frontend/src/pages/Otp.js
@@ -10,7 +10,7 @@ const Otp = () => {
   const handleVerifyOtp = async () => {
     try {
       const response = await axios.post(
-        "https://user-auth-api-ecru.vercel.app/user/verifyOtp",
+        "https://user-auth-api-rust.vercel.app/user/verifyOtp",
         { email: location.state.email, otp }
       );
       if (response.data.verified) {
