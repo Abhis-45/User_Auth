e3b2d927e7d079d6dde8eaddd6948b1ff52d0f74
Abhishek Kumar <91116774+Abhis-45@users.noreply.github.com>
2025-02-08 03:07:48 +0530

    Update user.js

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -6,6 +6,8 @@ router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.post("/sendOtp", UserController.sendOtp);
 router.post("/verifyOtp", UserController.verifyOtp);
+router.post("/logout", UserController.logout);
 router.delete("/:email", UserController.deleteAccount);
 
 module.exports = router;
+
